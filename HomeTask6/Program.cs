19cb0a33b0e7cabad64f742e88cb2e7fb0c74cf7
mammadlinurlan <nurlanym@code.edu.az>
2021-10-05 00:28:48 +0400

    Weapon Task Final

---
+++ HomeTask6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Weapon;
+
+namespace HomeTask6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert ammo");
+            int ammo = Int32.Parse(Console.ReadLine());
+
+            Weapon.Weapon weapon1 = new Weapon.Weapon(ammo);
+
+            Console.WriteLine("Shooting mode : auto|single");
+            string shootingMode = Console.ReadLine();
+
+            weapon1.Shoot(shootingMode);
+
+        }
+    }
+}
---
+++ Weapon/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Weapon
+{
+    public class Weapon
+    {
+        private int _ammo;
+
+        public Weapon(int ammo)
+        {
+            this._ammo = ammo;
+        }
+
+        public void Shoot(string shootingMode)
+        {
+            if (_ammo >= 0 && _ammo <= 30)
+            {
+                if (shootingMode == "auto" || shootingMode == "single")
+                {
+                    if (shootingMode == "auto")
+                    {
+                        while (_ammo > 0)
+                        {
+                            _ammo--;
+                            Console.WriteLine($"Shooted : Now your ammo = {_ammo}");
+                        }
+                        {
+                            Console.WriteLine($"You have shooted all of your ammo. Your ammo = {_ammo} Reload please");
+                            Console.WriteLine("insert ammo count for reload");
+                            int reloadammo = Int32.Parse(Console.ReadLine());
+                            if (_ammo + reloadammo <= 30)
+                            {
+                                _ammo += reloadammo;
+                                Console.WriteLine($"you have reloaded. Now your ammo = {_ammo}");
+                                Shoot(shootingMode);
+                                return;
+                            }
+                            Console.WriteLine("not enough space for that reloud ammo count");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        if (_ammo > 0)
+                        {
+                            _ammo--;
+                            Console.WriteLine($"You had a single shot , now your ammo = {_ammo}");
+                            Console.WriteLine("do you want to shoot again?  : yes || no");
+                            string answer = Console.ReadLine();
+                            if (answer == "yes" || answer == "no")
+                            {
+                                if (answer == "yes")
+                                {
+                                    Shoot(shootingMode);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("----------------END-----------------");
+                                }
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not ammo , please reload. Insert reload count");
+                            int reloadammo = Int32.Parse(Console.ReadLine());
+                            if (_ammo + reloadammo <= 30)
+                            {
+                                _ammo += reloadammo;
+                                Console.WriteLine($"you have reloaded. Now your ammo = {_ammo}");
+
+                                Shoot(shootingMode);
+                            }
+                            else
+                            {
+                                Console.WriteLine("not enough space for that reloud ammo count");
+                            }
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ShootingMode must be auto or single");
+                    return;
+                }
+            }
+            Console.WriteLine("ammo must be between 0 and 30");
+        }
+    }
+}
+
+
